9222d85867c1116a4d0e23db07a4caf88e728ff5
char-lie <5962274+char-lie@users.noreply.github.com>
2018-04-19 00:56:30 +0300

    Rename 'norm' to 'length' to be consistent with OpenCL names

--- src/raytracer/cpu.cpp
+++ src/raytracer/cpu.cpp
@@ -19,14 +19,14 @@ cl_float3 operator-(const cl_float3& x, const cl_float3& y)
     return {x.s[0] - y.s[0], x.s[1] - y.s[1], x.s[2] - y.s[2]};
 }
 
-float norm(const cl_float3& x)
+float length(const cl_float3& x)
 {
     return sqrt(dot(x, x));
 }
 
 float distance(const cl_float3& x, const cl_float3& y)
 {
-    return norm(x - y);
+    return length(x - y);
 }
 
 float dot(const cl_float3& x, const cl_float3& y)
--- src/raytracer/cpu.hpp
+++ src/raytracer/cpu.hpp
@@ -3,7 +3,7 @@
 cl_float3 operator-(const cl_float3& x, const cl_float3& y);
 cl_float3 operator-(const cl_float3& x, float c);
 cl_float3 operator*(const cl_float3& x, const float c);
-float norm(const cl_float3& x);
+float length(const cl_float3& x);
 float distance(const cl_float3& x, const cl_float3& y);
 float dot(const cl_float3& x, const cl_float3& y);
 
