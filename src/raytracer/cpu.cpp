9476269e5326e8789c8438175e311714d8ba0776
char-lie <5962274+char-lie@users.noreply.github.com>
2018-04-19 23:25:58 +0300

    Add 'max' and 'min' functions to 'cpu' module and use float4 for functions

--- src/raytracer/cpu.cpp
+++ src/raytracer/cpu.cpp
@@ -4,50 +4,74 @@
 
 #include "cpu.hpp"
 
-cl_float3 operator*(const cl_float3& x, const float c)
+cl_float4 operator*(const cl_float4& x, const float c)
 {
-    return {x.s[0] * c, x.s[1] * c, x.s[2] * c};
+    return
+        {x.s[0] * c, x.s[1] * c, x.s[2] * c, x.s[3] / c};
 }
 
-cl_float3 operator/(const cl_float3& x, const float c)
+cl_float4 operator/(const cl_float4& x, const float c)
 {
-    return {x.s[0] / c, x.s[1] / c, x.s[2] / c};
+    return
+        {x.s[0] / c, x.s[1] / c, x.s[2] / c, x.s[3] / c};
 }
 
-cl_float3& operator/=(cl_float3& x, const float c)
+cl_float4& operator/=(cl_float4& x, const float c)
 {
     x.s[0] /= c;
     x.s[1] /= c;
     x.s[2] /= c;
+    x.s[3] /= c;
     return x;
 }
 
-cl_float3 operator-(const cl_float3& x, float c)
+cl_float4 operator-(const cl_float4& x, float c)
 {
-    return {x.s[0] - c, x.s[1] - c, x.s[2] - c};
+    return
+        {x.s[0] - c, x.s[1] - c, x.s[2] - c};
 }
 
-cl_float3 operator-(const cl_float3& x, const cl_float3& y)
+cl_float4 operator-(const cl_float4& x, const cl_float4& y)
 {
-    return {x.s[0] - y.s[0], x.s[1] - y.s[1], x.s[2] - y.s[2]};
+    return
+        {x.s[0] - y.s[0], x.s[1] - y.s[1], x.s[2] - y.s[2], x.s[3] - y.s[3]};
 }
 
-cl_float3 operator+(const cl_float3& x, const cl_float3& y)
+cl_float4 operator+(const cl_float4& x, const cl_float4& y)
 {
-    return {x.s[0] + y.s[0], x.s[1] + y.s[1], x.s[2] + y.s[2]};
+    return
+        {x.s[0] + y.s[0], x.s[1] + y.s[1], x.s[2] + y.s[2], x.s[3] + y.s[3]};
 }
 
-float length(const cl_float3& x)
+cl_float4& operator+=(cl_float4& x, const cl_float4& y)
+{
+    x.s[0] += y.s[0];
+    x.s[1] += y.s[1];
+    x.s[2] += y.s[2];
+    x.s[3] += y.s[3];
+    return x;
+}
+
+float length(const cl_float4& x)
 {
     return sqrt(dot(x, x));
 }
 
-float distance(const cl_float3& x, const cl_float3& y)
+float distance(const cl_float4& x, const cl_float4& y)
 {
     return length(x - y);
 }
 
-float dot(const cl_float3& x, const cl_float3& y)
+float dot(const cl_float4& x, const cl_float4& y)
 {
-    return x.s[0] * y.s[0] + x.s[1] * y.s[1] + x.s[2] * y.s[2];
+    return
+        x.s[0] * y.s[0] + x.s[1] * y.s[1] + x.s[2] * y.s[2];
+}
+
+float min(float x, float y) {
+    return x < y ? x : y;
+}
+
+float max(float x, float y) {
+    return x > y ? x : y;
 }
--- src/raytracer/cpu.hpp
+++ src/raytracer/cpu.hpp
@@ -1,13 +1,16 @@
 #include <CL/cl.hpp>
 
-cl_float3 operator-(const cl_float3& x, const cl_float3& y);
-cl_float3 operator+(const cl_float3& x, const cl_float3& y);
-cl_float3 operator-(const cl_float3& x, float c);
-cl_float3 operator*(const cl_float3& x, const float c);
-cl_float3 operator/(const cl_float3& x, const float c);
-cl_float3& operator/=(cl_float3& x, const float c);
-float length(const cl_float3& x);
-float distance(const cl_float3& x, const cl_float3& y);
-float dot(const cl_float3& x, const cl_float3& y);
+cl_float4 operator-(const cl_float4& x, const cl_float4& y);
+cl_float4 operator+(const cl_float4& x, const cl_float4& y);
+cl_float4 operator-(const cl_float4& x, float c);
+cl_float4 operator*(const cl_float4& x, const float c);
+cl_float4 operator/(const cl_float4& x, const float c);
+cl_float4& operator/=(cl_float4& x, const float c);
+cl_float4& operator+=(cl_float4& x, const cl_float4& y);
+float length(const cl_float4& x);
+float distance(const cl_float4& x, const cl_float4& y);
+float dot(const cl_float4& x, const cl_float4& y);
+float min(float x, float y);
+float max(float x, float y);
 
 #define convert_float(x) static_cast<float>(x)
